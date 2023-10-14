bb5ec05e380c0ddde8ecd8b4c7ad959b5cdeb82c
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-10-14 12:17:30 +0300

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listElemet = document.getElementById('ingredients');
+const listFragment = document.createDocumentFragment();
+
+ingredients.forEach((ingredient) => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  listFragment.appendChild(item);
+});
+
+listElemet.appendChild(listFragment);
